50e1bd1340b0a9bc40178ef49dd63742041ddba8
Rowdenn <78979808+Rowdenn@users.noreply.github.com>
2021-03-19 02:31:42 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,54 +1,69 @@
-const { Client } = require('discord.js');
+const { Client, Collection } = require('discord.js');
 const { PREFIX } = require('./config');
-const client = new Client();
+const { readdirSync } = require("fs");
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+const client = new Client();
+["commands", "cooldowns"].forEach(x => client[x] = new Collection());
+
+const loadCommands = (dir = "./commands/") => {
+    readdirSync(dir).forEach(dirs => {
+        const commands = readdirSync(`${dir}/${dirs}/`).filter(files => files.endsWith('.js'));
+
+        for (const file of commands) {
+            const getFileName = require(`${dir}/${dirs}/${file}`);
+            client.commands.set(getFileName.help.name, getFileName);
+            console.log(`commande chargée: ${getFileName.help.name}`);
+        };
+    });
+};
+
+loadCommands();
 
 client.on('message', message => {
     if (!message.content.startsWith(PREFIX) || message.author.bot) return;
     const args = message.content.slice(PREFIX.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-    let pseudo = "";
-    let champion = "";
+    const commandName = args.shift().toLowerCase();
 
-    if (command === 'server') message.channel.send(`Je suis sur le serveur ${message.guild.name}.`);
-    if (command === 'user') message.channel.send(`Je suis l'utilisateur ${message.author.tag}.`);
-    if (command === 'opgg') { 
-        for(let i = 0; i < args.length; i++) {
-            pseudo += args[i];
-            if (i != args.length - 1) {
-                pseudo += "+"
-            }
-        };
-        message.channel.send(`https://euw.op.gg/summoner/userName=${pseudo}`);
-    };
-    if (command === 'build') { 
-        for(let i = 0; i < args.length; i++) {
-            champion += args[i];
-        };
-        message.channel.send(`https://probuilds.net/champions/details/${champion}`);
-    };
-    if (command === 'qiqi') {
-        const phrasesQiqi = [
-            "C'est OP",
-            "Go NERF",
-            "SALOPE",
-            "Hé viens 1v1 LA VRAIMENT",
-            "La juiffannnnce",
-            "Ta mère la juive",
-            "Le noir il court vite",
-            "Oui la vraiment",
-            "Suce ma grosse bite de noir",
-            "AH BOOOOOOOOOOOOOOON",
-            "Go ff je sais même pas pourquoi je joue",
-            "J'AI PAS DE TEAM",
-            "Hé ferme là"
-        ];
-        const reponse = phrasesQiqi[Math.floor(Math.random() * phrasesQiqi.length)];
-        message.channel.send(reponse);
-    };
+    if (!client.commands.has(commandName)) return message.channel.send("Elle existe pas cette commande sale fdp");
+    const command = client.commands.get(commandName);
+
+    if (command.help.args && !args.length) {
+        let noArgsReply = `Il me faut des arguments pour cette commande, ${message.author}!`;
+
+        if (command.help.usage) noArgsReply += `\nVoici comment utiliser la commande: \`${PREFIX}${command.help.name} ${command.help.usage}\``
+
+        return message.channel.send(noArgsReply);
+    }
+
+    if (!client.cooldowns.has(command.help.name)) {
+        client.cooldowns.set(command.help.name, new Collection());
+    }
+
+    const timeNow = Date.now();
+    const tStamps = client.cooldowns.get(command.help.name);
+    const cdAmount = (command.help.cooldown || 5) * 1000;
+    console.log(client.cooldowns);
+
+    if (tStamps.has(message.author.id)) {
+        const cdExpirationTime = tStamps.get(message.author.id) + cdAmount;
+
+        if (timeNow < cdExpirationTime) {
+            timeLeft = (cdExpirationTime - timeNow) / 1000;
+            return message.reply(`Tu pourras retaper la commande \`${command.help.name}\` dans ${timeLeft.toFixed(0)} seconde(s).`);
+        }
+    }
+
+    tStamps.set(message.author.id, timeNow);
+    setTimeout(() => tStamps.delete(message.author.id), cdAmount);
+
+    command.run(client, message, args);
 });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity("JVLIVS II", {
+        type: "LISTENING"
+    });
+  });
 
 client.login(process.env.TOKEN);
