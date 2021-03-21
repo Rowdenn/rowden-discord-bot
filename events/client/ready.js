c063cdc9de493877877a00fda6486474284e1ab2
Rowdenn <78979808+Rowdenn@users.noreply.github.com>
2021-03-21 16:33:04 +0100

    Delete events/client directory

--- events/client/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = (client) => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.channels.cache.get('822491300569808966').send("Je suis en ligne");
-    client.user.setActivity("JVLIVS II", {
-        type: "LISTENING"
-    });
-}
